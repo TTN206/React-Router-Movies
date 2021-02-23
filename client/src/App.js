8296b60ef85a101c828bf2e958c38ece38f59cb3
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-02-23 08:14:58 -0800

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  // const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState( [] );
 
   useEffect(() => {
@@ -17,6 +17,9 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          // console.log( response.data );
+          setMovieList( response.data );
+
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -25,9 +28,9 @@ export default function App () {
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  // const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+  // };
 
   return (
     <div>
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function MovieList( props ) {
   return (
@@ -11,17 +12,21 @@ export default function MovieList( props ) {
 }
 
 function MovieDetails(props) {
+  
   const { title, director, metascore, id } = props.movie;
   console.log( id );
-  
+
+  const history = useHistory();
+  console.log( history );
+
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
+    <div className="movie-card" onClick = { () => history.push( `/movies/${id}` ) }>
+      <h2>{ title }</h2>
       <div className="movie-director">
-        Director: <em>{director}</em>
+        Director: <em>{ director }</em>
       </div>
       <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
+        Metascore: <strong>{ metascore }</strong>
       </div>
     </div>
   );
