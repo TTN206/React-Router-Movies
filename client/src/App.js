5cc0102ecd662200895578c5451f8b3bb14a2856
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-02-22 21:40:23 -0800

    save for canvas

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Link, Switch } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+  const [movieList, setMovieList] = useState( [] );
 
   useEffect(() => {
     const getMovies = () => {
@@ -30,7 +33,17 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Switch>
+          <Route path = "/movies/:id" >
+            <Movie />
+          </Route>
+
+          <Route path = "/">
+            <MovieList movies = { movieList } />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-export default function MovieList(props) {
+export default function MovieList( props ) {
   return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+    <div className = "movie-list">
+      { props.movies.map(movie => (
+        <MovieDetails key = { movie.id } movie = { movie } />
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
+  const { title, director, metascore, id } = props.movie;
+  console.log( id );
+  
   return (
     <div className="movie-card">
       <h2>{title}</h2>
